fa20eab86b760fdafb44133db9e6e90fc5f8a6c8
dilane-ts <lefakongdilane@gmail.com>
2025-05-19 19:12:18 +0100

    First commit

---
+++ src/test/java/com/ticky/ticky/TickyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticky.ticky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TickyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
